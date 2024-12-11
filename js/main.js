2c9a5568ffd0e5a16988b2719daa9f980d451020
Ian Maksimovic <ianmaksimovic@gmail.com>
2024-12-11 15:31:55 -0500

    numbers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-const myVariable = "Mathematics";
+const myNumber = 42;
 
-console.log("John,Joe,Mike".split(","))
+const myFloat = 42.01;
+
+const myString = "42.021abc";
+const notnumber = NaN;
+
+console.log(Number.parseFloat(myString).toFixed(2).toString());
+
+console.log(isNaN("Dave")); // Checks if it isnt a number
+
+console.log(Number.isNaN("Dave")); // Checks whether it is NaN datatype
+
+console.log(Number.isNaN(notnumber))
