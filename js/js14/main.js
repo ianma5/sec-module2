c7c041cc45111bd00f0012991f52e0650f34054b
Ian Maksimovic <ianmaksimovic@gmail.com>
2024-12-18 21:46:06 -0500

    scope

---
+++ js/js14/main.js
@@ -0,0 +1,32 @@
+ /*
+// var, let, and const
+
+// no error
+var x= 1;
+var x= 2;
+console.log(x);
+
+// no error unless use let twice
+let x= 1;
+x= 2;
+console.log(x);
+
+// error
+const x= 1;
+x= 2;
+console.log(x); */
+
+
+/*
+function myFunc() {
+    const z= 5;
+    console.log(z);
+    // local scope
+    { 
+        let y=4;
+        console.log(y);
+    }
+}
+
+myFunc(); */
+
