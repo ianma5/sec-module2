e559500afe537aa842ac8c45a58624785b531858
Ian Maksimovic <ianmaksimovic@gmail.com>
2024-12-21 16:48:41 -0500

    error handling

---
+++ js/js20/main.js
@@ -0,0 +1,26 @@
+// error handling
+"use strict";
+
+const makeError = () => {
+    let i = 1;
+    while (i <= 5) {
+        try {
+            if (i % 2 !==0) {
+                throw new Error("Odd number!");
+            }
+            console.log("Even number");
+        } catch(err) {
+            console.error(err.stack);
+        } finally {
+            console.log("...finally");
+            i++;
+        }
+    }
+}
+makeError();
+
+function customError(message) {
+    this.message = message;
+    this.name = "customError";
+    this.stack = `${this.name}: ${this.message}`;
+}
