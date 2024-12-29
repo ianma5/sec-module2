6716e7d233c32cd0df2fd4b11110c9fe833042d9
Ian Maksimovic <ianmaksimovic@gmail.com>
2024-12-28 20:21:06 -0500

    web storage

---
+++ js/js23/main.js
@@ -0,0 +1,22 @@
+// web storage API - not part of the DOM, use window API
+
+/* window.alert(window.location); // window is "implied"
+alert(location);
+ */
+const myArray = ["eat", "sleep", "code"];
+
+const myObject = {
+    name: "Dave",
+    hobbies: ["eat", "sleep", "code"],
+    logName: function() {
+        console.log(this.name);
+    }
+}
+
+myObject.logName();
+
+localStorage.setItem("myLocalStore", JSON.stringify(myArray)); // turn into json
+const storeLength = localStorage.length;
+const myLocalStore = JSON.parse(localStorage.getItem("myLocalStore")); // gets object back
+
+console.log(storeLength);
